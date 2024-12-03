75a4d75a507a50e6011741daecce8ce8c20e74c5
harshavardhan.r <harshavardhan.r@GSPSFJ9S542>
2024-12-03 14:16:23 +0530

    task complete

--- CAtasks/Task5_Arrays_1.cs
+++ CAtasks/Task5_Arrays_1.cs
@@ -1,27 +1,66 @@
 ﻿using System;
-using System.Collections.Generic;
-using System.Linq;
-using System.Text;
-using System.Threading.Tasks;
+using System.ComponentModel.Design;
 
 namespace CAtasks
 {
-    internal class Task5_Arrays_1
+    class Task5_Arrays_1
     {
         static void Main(string[] args)
         {
-            int[,] ar = new int[3, 4] { { 12, 34, 45, 56 }, { 45, 56, 78, 98 }, { 12, 32, 43, 54 } };
-
-            Console.WriteLine("Elements of Array are: ");
-            for (int i = 0; i < 3; i++)
+            Console.WriteLine($"Choose Which Type Of Array You Want To Store Your Numbers in...!");
+            Selection:
+            Console.WriteLine($"1. One Dimensional Array\n2. Two Dimensional Array\nPress 1 or 2 to Continue...");
+            short type = Convert.ToInt16(Console.ReadLine());
+            if (type == 1)
             {
-                for (int j = 0; j < 4; j++)
+                Console.WriteLine($"Enter Size Of One Dimensional Array: ");
+                int size = Convert.ToInt32(Console.ReadLine());
+                int[] array = new int[size];
+                Console.WriteLine($"Enter Elements In The One Dimensional Array: ");
+                for (int i = 0; i < size; i++)
                 {
-                    Console.Write(ar[i, j] + "  ");
+                    Console.Write("Enter Element - " + (i + 1) + ":");
+                    array[i] = Convert.ToInt32(Console.ReadLine());
+                    Console.WriteLine();
                 }
-                Console.WriteLine();
+                Console.WriteLine("Elements of Array are: ");
+                foreach (var item in array)
+                {
+                    Console.Write(item + ", ");
+                }
             }
-
+            else if (type == 2)
+            {
+                Console.WriteLine($"Enter Size Of Two Dimensional Array in Row/Col...! ");
+                Console.WriteLine($"Enter Row Size Of Two Dimensional Array: ");
+                int rowSize = Convert.ToInt32(Console.ReadLine());
+                Console.WriteLine($"Enter Column Size Of Two Dimensional Array: ");
+                int colSize = Convert.ToInt32(Console.ReadLine());
+                int[,] array = new int[rowSize, colSize];
+                Console.WriteLine($"Enter Elements in the Array..");
+                for (int i = 0; i < rowSize; i++)
+                {
+                    for (int j = 0; j < colSize; j++)
+                    {
+                        Console.Write($"Enter Element {i + 1}x{j + 1}: ");
+                        array[i, j] = Convert.ToInt32(Console.ReadLine());
+                    }
+                }
+                Console.WriteLine("Elements of Array are: ");
+                for (int i = 0; i < rowSize; i++)
+                {
+                    for (int j = 0; j < colSize; j++)
+                    {
+                        Console.Write(array[i, j] + "  ");
+                    }
+                    Console.WriteLine();
+                }
+            }
+            else
+            {
+                Console.WriteLine($"Please Select Valid Array Dimension From the Given Option's");
+                goto Selection;
+            }
             Console.Read();
         }
     }
