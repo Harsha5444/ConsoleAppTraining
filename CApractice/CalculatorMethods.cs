e51122882988cc1492d6c022250c21654edf259a
harshavardhan.r <harshavardhan.r@GSPSFJ9S542>
2024-12-10 10:29:54 +0530

    .

---
+++ CApractice/CalculatorMethods.cs
@@ -0,0 +1,51 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace CApractice
+{
+    class CalculatorMethods
+    {
+        // Create methods for:
+        // 1. Add two numbers
+        // 2. Subtract two numbers
+        // 3. Multiply two numbers
+        // 4. Divide two numbers
+
+        public int Add(int a, int b)
+        {
+            return a + b;
+        }
+        public int Sub(int a , int b)
+        {
+            return (a - b);
+        }
+        public int Mutiply(int a , int b)
+        {
+            return (a * b);
+        }
+        public int div(int a , int b)
+        {
+            return (a / b);
+        }
+    }
+
+    class execute
+    {
+        static void Main(string[] args)
+        {
+            CalculatorMethods addition = new CalculatorMethods();
+            CalculatorMethods subtraction = new CalculatorMethods();
+            CalculatorMethods multiplication = new CalculatorMethods();
+            CalculatorMethods division = new CalculatorMethods();
+            int ans1 = addition.Add(10, 30);
+            subtraction.Sub(10, 30);
+            multiplication.Mutiply(10, 30); 
+            division.div(10, 30);
+            Console.WriteLine(ans1);
+            Console.Read();
+        }
+    }
+}
--- CApractice/Practice_Dummy.cs
+++ CApractice/Practice_Dummy.cs
@@ -1,18 +1,48 @@
 ﻿using System;
-using System.CodeDom.Compiler;
-using System.Collections.Generic;
-using System.Collections.ObjectModel;
-using System.Linq;
-using System.Text;
-using System.Threading.Tasks;
 
 namespace CApractice
 {
-    internal class Practice_Dummy
+    //Class
+    class Employee
+    {
+        int empId; string empName; double salary;
+        string city; string state;string role; string depot;int id;
+
+        public Employee(int empId, string empName, double salary , Employee loc)
+        {
+            this.empId = empId;
+            this.empName = empName;
+            this.salary = salary;
+            this.city = loc.city;
+            this.state = loc.state;
+        }
+
+        public Employee (String city , String state)
+        {
+            this.city = city;   
+            this.state = state;
+        }
+        public void Display()
+        {
+            Console.WriteLine($"EmpID: {empId}, EmpName: {empName} and Salary: {salary}");
+            Console.WriteLine($"City: {city} and State: {state}");
+        }
+    }
+
+    class Program
     {
         static void Main(string[] args)
         {
+            Employee loc = new Employee("HYD","Ts");
 
+            Employee emp1 = new Employee(10,"harsha",1089365.00,loc);
+            emp1.Display();
+
+            Employee emp2 = new Employee(12, "haredfawefsha", 10543.00, loc);
+            emp2.Display();
+
+
+            Console.Read();
         }
     }
 }
